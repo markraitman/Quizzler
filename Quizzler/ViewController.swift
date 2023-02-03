debae1dc80d9782f634ea97616ce4aac1125d801
Mark Raitman <markraitman@mail.ru>
2023-02-03 11:31:15 +0300

    Добавлен вопросник от Анжелы, также начата обработка цвета кнопок при нажатии для правильного и неправильного ответов

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -8,24 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
-    //Default Quiz
-    //
-    //  Question(q: "A slug's blood is green.", a: "True"),
-    //        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-    //        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-    //        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-    //        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-    //        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-    //        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-    //        Question(q: "Google was originally called 'Backrub'.", a: "True"),
-    //        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-    //        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-    //        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-    //        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-    //
-    //
+
     //Multiple Choice Quiz
     //
     // Question(q: "Which is the largest organ in the human body?", a: ["Heart", "Skin", "Large Intestine"], correctAnswer: "Skin"),
@@ -41,9 +24,18 @@ class ViewController: UIViewController {
     
     //MARK: Properties
     let quiz = [
-        Question(q: "True story", a: "True"),
-        Question(q: "False story", a: "True"),
-        Question(q: "Total b*llshit", a: "False")
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
     ]
     
     var questionNumber = 0
@@ -69,9 +61,9 @@ class ViewController: UIViewController {
         let userAnswer = quiz[questionNumber].answer
         
         if questionAnswer == userAnswer {
-            print("Right!")
+            sender.backgroundColor = UIColor.green
         } else {
-            print("Wrong!")
+            sender.backgroundColor = UIColor.red
         }
         
         if questionNumber + 1 < quiz.count {
@@ -87,6 +79,8 @@ class ViewController: UIViewController {
     
     func updateUI() {
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
     }
     
 }
